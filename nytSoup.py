6155fd838e17ab31c05236e0c0e0868078ddfebc
prbsh9 <prbsh9@gmail.com>
2020-03-29 16:42:37 +0545

    middle

--- nytSoup.py
+++ nytSoup.py
@@ -1,6 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
 
-nyt = requests.get("https://www.nytimes.com/").text
-nytNews = BeautifulSoup(nyt, "html.parser")
-print(nytNews.title.text)
+
+def wired_scrape():
+    wired = requests.get("https://www.wired.com/category/security/")
+    # wired = wired.
+    wiredSoup = BeautifulSoup(wired.text, "html.parser")
+
+    # title = wiredSoup.title.text
+    news = wiredSoup.find_all("li", class_="card-component__description")
+    for x in news:
+        heading = x.h2.text
+        link = "https://www.wired.com" + x.a.get("href")
+        paragraph = pWired(link)
+        print(heading)
+        print(paragraph)
+
+
+def pWired(link):
+    paraHtml = requests.get(link).text
+    paraSoup = BeautifulSoup(paraHtml, "html.parser")
+
+    paras = paraSoup.find_all("p")
+    para_div = paraSoup.find("div", class_="article__body").p
+    for x in paras:
+        a = x.text
+        print()
+    print()
+    return paras[:2]
+
+
+wired_scrape()
+
